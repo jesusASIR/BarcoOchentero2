1f5a1ff96eaeccbdf250b20fb53ca0c91ecb47cd
yisus <jesus.clase.g.p@gmail.com>
2020-06-09 22:01:16 +0200

    Initial Commit

---
+++ BarcoOchentero2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcoOchentero2
+//
+//  Created by yisus on 09/06/2020.
+//  Copyright © 2020 yisus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BarcoOchentero2Tests/BarcoOchentero2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BarcoOchentero2Tests.swift
+//  BarcoOchentero2Tests
+//
+//  Created by yisus on 09/06/2020.
+//  Copyright © 2020 yisus. All rights reserved.
+//
+
+import XCTest
+@testable import BarcoOchentero2
+
+class BarcoOchentero2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
